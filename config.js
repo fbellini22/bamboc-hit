6f3861ba13b531423fd58d26dbbe2889829c505a
Filippo Bellini <fbellini22@gmail.com>
2026-05-10 12:20:52 +0200

    Fix GitHub Pages root setup and Spotify playback flow

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
+function getRedirectUri() {
+  if (window.location.hostname === "fbellini22.github.io") {
+    return "https://fbellini22.github.io/bamboc-hit/";
+  }
+
+  return window.location.origin;
+}
+
 const CONFIG = {
   CLIENT_ID: "1031669a52cf4742b6e908a536a247e5",
 
-  REDIRECT_URI: window.location.origin,
+  REDIRECT_URI: getRedirectUri(),
 
   SCOPES: [
     "user-read-email",
